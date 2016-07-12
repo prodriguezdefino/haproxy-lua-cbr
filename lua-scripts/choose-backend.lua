a82baee0e4bb106be4cea318dfb9b2880edc4122
pablo rodriguez defino <prodriguezdefino@gmail.com>
2016-07-11 17:39:49 -0700

    added a table with server names

--- lua-scripts/choose-backend.lua
+++ lua-scripts/choose-backend.lua
@@ -1,3 +1,11 @@
+-- initialize server maping
+servers = {
+  prim = 'nodes-prim',
+  sec = 'nodes-sec',
+  default = 'default'
+}
+
+-- helps to decode query parameters presents in the url
 function urldecode(s)
   s = s:gsub('+', ' ')
        :gsub('%%(%x%x)', function(h)
@@ -6,7 +14,8 @@ function urldecode(s)
   return s
 end
 
-function parseurl(s)
+-- parses the query parameters and produces a table with keys and values
+function parseQueryParams(s)
   local ans = {}
   for k,v in s:gmatch('([^&=?]-)=([^&=?]+)' ) do
     ans[ k ] = urldecode(v)
@@ -14,22 +23,27 @@ function parseurl(s)
   return ans
 end
 
+-- checks for empty strings
 function isempty(s)
   return s == nil or s == ''
 end
 
+-- selects the backend server farms to use base on a GET request content
 function resolveBackendForGET(txn)
-  local server = "nodes-prim"
+  local server = servers.default
   local queryps = txn.sf:query()
   if not isempty(queryps) then
-    local qparams = parseurl(queryps)
+    local qparams = parseQueryParams(queryps)
     if not isempty(qparams.server) then
-      server = qparams.server
+      if servers[qparams.server] ~= nil then 
+        server = servers[qparams.server]
+      end
     end
   end
   return server
 end
 
+-- to be register fetch function 
 function choose_backend(txn)
   txn:Info("***********************************************")
   txn:Info("Request coming from: " .. txn.f:src())
@@ -39,7 +53,7 @@ function choose_backend(txn)
   txn:Info("Request query parameters: " .. txn.sf:query())
   txn:Info("***********************************************")
   
-  local server = "nodes-prim"
+  local server = servers.default
   local method = txn.sf:method()
   if method == 'GET' then
     server = resolveBackendForGET(txn)    
@@ -47,4 +61,5 @@ function choose_backend(txn)
   return server
 end
 
+-- register the fetch function
 core.register_fetches("choose-backend", choose_backend)
